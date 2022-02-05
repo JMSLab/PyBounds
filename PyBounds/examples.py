78eec91bea4f469489b82dde42981128be7d08c7
Nathan Sun <nsun@college.harvard.edu>
2022-02-05 17:18:07 -0500

    Break apart lines that are too long

--- PyBounds/examples.py
+++ PyBounds/examples.py
@@ -4,7 +4,8 @@ import io
 import pyBounds
 
 # import data: p_t, q_t
-url = "https://raw.githubusercontent.com/JMSLab/PyBounds/870170743f155b3a9da680ec64e0d5079e266601/PyBounds/roberts_schlenker_2013.csv?token=GHSAT0AAAAAABQYAQ7N7L2PIVTKPDHHG7WWYQEWC6A"
+url = "https://raw.githubusercontent.com/JMSLab/PyBounds/870170743f155b3a9da680ec64e0d5079e266601/PyBounds" \
+      "/roberts_schlenker_2013.csv?token=GHSAT0AAAAAABQYAQ7N7L2PIVTKPDHHG7WWYQEWC6A"
 download = requests.get(url).content
 
 data = pd.read_csv(io.StringIO(download.decode('utf-8')))
