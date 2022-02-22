12ba79852b7e7a46c6f7aa71564aab9ed4e62621
Nathan Sun <nsun@college.harvard.edu>
2022-02-22 00:44:04 -0500

    Method Implementation
    First draft of the code for the package. The shape of the graph produced looks similar to the one in the paper, although the \underline{theta} values aren't quite right yet.

--- PyBounds/examples.py
+++ PyBounds/examples.py
@@ -1,18 +1,18 @@
+import numpy as np
 import pandas as pd
 import requests
 import io
 import pyBounds
 
 # import data: p_t, q_t
-url = "https://raw.githubusercontent.com/JMSLab/PyBounds/870170743f155b3a9da680ec64e0d5079e266601/PyBounds" \
-      "/roberts_schlenker_2013.csv?token=GHSAT0AAAAAABQYAQ7N7L2PIVTKPDHHG7WWYQEWC6A"
+url = "https://raw.githubusercontent.com/JMSLab/PyBounds/master/PyBounds/roberts_schlenker_2013.csv?token=GHSAT0AAAAAABRP7ARWJWFITYQIO3DE42NMYQ5WEWQ"
 download = requests.get(url).content
 
 data = pd.read_csv(io.StringIO(download.decode('utf-8')))
 df = pd.DataFrame(data)
-p_t = df.iloc[:, 0]
-q_t = df.iloc[:, 1]
+p_t = df.iloc[:, 2]
+q_t = df.iloc[:, 3]
 
 if __name__ == '__main__':
-    bounds_data = pyBounds.Bounds(p_t, q_t)
-    print(bounds_data.delta_epsilon(5))
+    bounds_data = pyBounds.Bounds(p_t, q_t, k=np.inf, maxB=0.1)
+    print(bounds_data.plot())
--- PyBounds/pyBounds.py
+++ PyBounds/pyBounds.py
@@ -5,12 +5,16 @@
 # NEEDS TESTING!!!!!
 
 import numpy as np
+import scipy as scipy
+from scipy.optimize import minimize
+from matplotlib import pyplot as plt
 
 
 class Bounds:
     def __init__(self, p_t, q_t, k=np.inf, maxB=None, ngridpoints=1000):
         self.p = p_t
         self.q = q_t
+        self.ngridpoints = ngridpoints
         if k < 1:
             raise Exception("Requires k>=1")
         else:
@@ -19,7 +23,6 @@ class Bounds:
             self.maxB = 2 * self.underline_B()
         else:
             self.maxB = maxB
-        self.ngridpoints = ngridpoints
 
     # implementation of k-mean
 
@@ -33,13 +36,11 @@ class Bounds:
         value = 0
         length = len(vector)
         if k == np.inf:
-            for i in vector:
-                value = max(value, i)
-            return value
+            return np.max(vector)
         else:
             for i in vector:
                 value = value + i ** k
-            return ((1. / length) * value) ** (1 / k)
+            return ((1 / length) * value) ** (1 / k)
 
     # calculate Delta_epsilon
     def delta_epsilon(self, theta):
@@ -50,40 +51,82 @@ class Bounds:
             """
         return np.subtract(np.diff(self.q), theta * np.diff(self.p))
 
+    def M_hat_k(self, theta):
+        """
+        implementation of tilde{M_k}
+        used in underline_B
+        :return: the k-mean of delta-epsilon
+        """
+        return self.kmean(self.k, np.abs(self.delta_epsilon(theta)))
+
     def B_tilde(self):
         """
         calculates B_tilde used in underline{B} for the case k = infty; introduced in Proposition 1
         :return: B_tilde operation in the case of k=infty as defined in Proposition 1
         """
-        return
+        def f(B):
+            return self.overline_theta(B) - self.underline_theta(B)
+        return scipy.optimize.root_scalar(f, x0=0, x1=0.001).root
+
+    def underline_theta(self, B):
+        if self.k == np.inf:
+            theta_underline_values = []
+            for i in range(1, len(self.p)):
+                if self.p[i] - self.p[i - 1] != 0:
+                    delta_q_t = self.q[i] - self.q[i - 1]
+                    delta_p_t = self.p[i] - self.p[i - 1]
+                    theta_underline_values.append(delta_q_t / delta_p_t - B / (abs(delta_p_t)))
+            return max(theta_underline_values)
+
+        else:
+            def f(theta):
+                return self.M_hat_k(theta) - B
+
+            sol = scipy.optimize.root_scalar(f, x0=0, x1=-1)
+            return sol.root
+
+    def overline_theta(self, B):
+        if self.k == np.inf:
+            theta_overline_values = []
+            for i in range(1, len(self.p)):
+                if self.p[i] - self.p[i - 1] != 0:
+                    delta_q_t = self.q[i] - self.q[i - 1]
+                    delta_p_t = self.p[i] - self.p[i - 1]
+                    theta_overline_values.append(delta_q_t / delta_p_t + B / (abs(delta_p_t)))
+            return min(theta_overline_values)
+        else:
+            def f(theta):
+                return self.M_hat_k(theta) - B
+
+            sol = scipy.optimize.root_scalar(f, x0=0, x1=1)
+            return sol.root
 
     def underline_B(self):
         """
         case k = infinity uses B_tilde as defined above
         case k in (1, infinity): implement the definitions of underline{B} as stated in Proposition 1 and Proposition 2
-
         :return: returns the underline_{B_k} operation as defined in Propositions 1 and 2
         """
         if self.k == np.inf:
-            return 0
-            # temporary placeholder to ensure program runs
+            delta_q = []
+            for i in range(1, len(self.p)):
+                if self.p[i] - self.p[i - 1] == 0:
+                    delta_q.append(self.q[i] - self.q[i - 1])
+            if not delta_q:
+                return self.B_tilde()
+            else:
+                return max(max(delta_q), self.B_tilde())
         else:
-            return 0
-            # temporary placeholder to ensure program runs
+            # print(minimize(self.M_tilde_k, 0))
+            return minimize(self.M_hat_k, np.array([0])).fun
 
     def Theta_hat_k_B(self, B):
         """
         :param B: upper bound B provided
         :return: output [underline{theta}_k, overline{theta}_k] for a given value
             """
-        if self.k == np.inf:
-            # calculate \underline{\theta_k}
-            # calculate \overline{\theta_k}
-            # ^^ as defined in Prop. 1
-            return
-        else:
-            # calculate \underline{\theta_k}, \overline{\theta_k} as defined in Prop. 2
-            return
+        # print(self.underline_theta(B))
+        return [self.underline_theta(B), self.overline_theta(B)]
 
     def intervals(self):
         """
@@ -91,14 +134,27 @@ class Bounds:
         """
         array_of_intervals = []
         i = self.underline_B()
-        while i < self.maxB:
+        while i <= self.maxB:
             array_of_intervals.append(self.Theta_hat_k_B(i))
             i += (self.maxB - self.underline_B()) / self.ngridpoints
+        print(len(array_of_intervals))
         return array_of_intervals
 
     def plot(self):
         """
         :return: outputs a plot of B vs. theta similar to Figure 3
         """
-        # returns a plot of B vs. \theta similar to Figure 3
-        return
+        intervals = self.intervals()
+        lower_bounds = []
+        upper_bounds = []
+        x = []
+        i = self.underline_B()
+        while i <= self.maxB:
+            x.append(i)
+            lower_bounds.append(self.underline_theta(i))
+            upper_bounds.append(self.overline_theta(i))
+            i = i + (self.maxB-self.underline_B())/self.ngridpoints
+        plt.plot(x, lower_bounds, linewidth=.3)
+        # plt.plot(x, upper_bounds, linewidth=.3)
+        plt.savefig('test.png')
+        plt.show()
