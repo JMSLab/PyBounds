24c89fa3d3a1a714ce72bbbdaf35f27fe4573512
Nathan Sun <nsun@college.harvard.edu>
2022-03-07 23:18:21 -0500

    Automated workflow unit testing

--- PyBounds/test_main.py
+++ PyBounds/test_main.py
@@ -14,17 +14,17 @@ df = pd.DataFrame(data)
 p_t = df['logPD0']
 q_t = df['logD0']
 
+
 class Test(TestCase):
     def test_add(self):
         kinf = pyBounds.Bounds(p_t, q_t, k=np.inf, maxB=0.1)
-        self.assertAlmostEqual(kinf.Theta_hat_k_B(0.07)[0], -0.122187908332218, delta = 0.000001)
-        self.assertAlmostEqual(kinf.Theta_hat_k_B(0.07)[1], 0.134092308480860, delta = 0.000001)
+        self.assertAlmostEqual(kinf.Theta_hat_k_B(0.07)[0], -0.122187908332218, delta=0.000001)
+        self.assertAlmostEqual(kinf.Theta_hat_k_B(0.07)[1], 0.134092308480860, delta=0.000001)
         k2 = pyBounds.Bounds(p_t, q_t, k=2, maxB=0.04)
-        self.assertAlmostEqual(k2.Theta_hat_k_B(0.03)[0], -0.123518421564038, delta = 0.000001)
-        self.assertAlmostEqual(k2.Theta_hat_k_B(0.03)[1], 0.106798443278028, delta = 0.000001)
+        self.assertAlmostEqual(k2.Theta_hat_k_B(0.03)[0], -0.123518421564038, delta=0.000001)
+        self.assertAlmostEqual(k2.Theta_hat_k_B(0.03)[1], 0.106798443278028, delta=0.000001)
         return
 
 
 if __name__ == '__main__':
     main()
-
